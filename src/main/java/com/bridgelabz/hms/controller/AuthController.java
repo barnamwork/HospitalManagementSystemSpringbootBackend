45d60700d54a13887db15b2ce77fb206348d7720
Barnamwork <barnamwork@gmail.com>
2026-04-06 13:04:25 +0530

    [Barnam] UC6: implemented JWT token service

--- src/main/java/com/bridgelabz/hms/controller/AuthController.java
+++ src/main/java/com/bridgelabz/hms/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.hms.controller;
 
+import com.bridgelabz.hms.config.JwtService;
 import com.bridgelabz.hms.entity.User;
 import com.bridgelabz.hms.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,21 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    // Register API
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/register")
     public String register(@RequestBody User user) {
         return authService.register(user);
     }
 
-    // Login API
     @PostMapping("/login")
     public String login(@RequestBody User user) {
 
         User existingUser = authService.login(user.getEmail(), user.getPassword());
 
         if (existingUser != null) {
-            return "Login successful";
+            return jwtService.generateToken(user.getEmail());
         }
 
         return "Invalid credentials";
